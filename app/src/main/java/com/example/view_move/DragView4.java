46a9b4eaf8749cea1211f15bc8d51bcea32579f1
Mike-Shake <1554367213@qq.com>
2023-03-15 21:30:25 +0800

    scrollBy方法

---
+++ app/src/main/java/com/example/view_move/DragView4.java
@@ -0,0 +1,52 @@
+package com.example.view_move;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DragView4 extends View {
+    public DragView4(Context context) {
+        super(context);
+        ininView();
+    }
+
+    public DragView4(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        ininView();
+    }
+
+    public DragView4(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        ininView();
+    }
+
+    private int lastX;
+    private int lastY;
+
+    public void ininView(){
+        setBackgroundColor(Color.RED);
+    }
+
+    //scrollBy方法
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x=(int) event.getX();
+        int y=(int) event.getY();
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                lastX=(int )event.getX();
+                lastY=(int)event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int offsetX=x-lastX;
+                int offsetY=y-lastY;
+                ((View)getParent()).scrollTo(-offsetX,-offsetY);
+                break;
+        }
+        return true;
+    }
+}
